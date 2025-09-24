88ff76ee4b5993360244aefe8717dc80461c403f
Aris <acwhitney@willamette.edu>
2025-09-24 14:45:47 -0700

    Remove build artifacts from repo

--- target/debug/deps/sqrt_lab-22279c0ff8c773ee.d
+++
@@ -1,5 +0,0 @@
-/home/acwhitney/271rs/sqrt_lab/target/debug/deps/sqrt_lab-22279c0ff8c773ee.d: src/main.rs
-
-/home/acwhitney/271rs/sqrt_lab/target/debug/deps/sqrt_lab-22279c0ff8c773ee: src/main.rs
-
-src/main.rs:
--- target/debug/sqrt_lab.d
+++
@@ -1,1 +0,0 @@
-/home/acwhitney/271rs/sqrt_lab/target/debug/sqrt_lab: /home/acwhitney/271rs/sqrt_lab/src/main.rs
