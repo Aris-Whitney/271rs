19e773ed7ff28b654f0bd1478d631c877441ac56
Aris <acwhitney@willamette.edu>
2025-09-25 14:45:24 -0700

    Initial Commit

---
+++ target/debug/libmacros.d
@@ -0,0 +1,1 @@
+/home/acwhitney/271rs/32/target/debug/libmacros.rlib: /home/acwhitney/271rs/32/src/lib.rs
---
+++ target/debug/macros.d
@@ -0,0 +1,1 @@
+/home/acwhitney/271rs/32/target/debug/macros: /home/acwhitney/271rs/32/src/lib.rs /home/acwhitney/271rs/32/src/main.rs
